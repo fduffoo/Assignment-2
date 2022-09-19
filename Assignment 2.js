5e660e41cfa28869d45a1181984d0ee61155ac57
Fernando Duffoo <89112257+fduffoo@users.noreply.github.com>
2022-09-19 19:17:23 -0400

    Add files via upload

---
+++ Assignment 2.js
@@ -0,0 +1,35 @@
+// Fernando Duffoo
+// INF651
+// 9/19/2022
+// Assignment 2
+
+// Question 1
+let myNumber;
+// Question 2
+myNumber = 5;
+// Question 3
+let myOtherNumber;
+myOtherNumber = 5;
+// Question 4
+let myTotal;
+myTotal = myNumber * myOtherNumber;
+// Question 5
+let myName;
+myName = "Fernando Duffoo";
+let myCombo;
+myCombo = "myNumber + myOtherNumber";
+// Question 6
+let myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+// Question 7
+let myRemainder;
+myRemainder = myNumber/5;
+// Question 8
+let i;
+i = 2;
+// Question 9
+let int;
+int = 4;
+// Question 10
+let myBoolean;
+myBoolean = false;
